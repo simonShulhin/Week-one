e9d6a4b59098fd7f9cf0564f08406c7d06863289
simonShulhin <42201346+simonShulhin@users.noreply.github.com>
2022-05-29 10:43:11 +0300

    Update task_three.js

--- task_three.js
+++ task_three.js
@@ -1,6 +1,8 @@
 function isValidParentheses(parens){
     while (parens.includes('()')) { 
+        // good practice не змінювати аргументи функції
       parens = parens.replace('()', '');
     }
+    // використовуй строге порівняння ===
     return (parens == '');
   }
